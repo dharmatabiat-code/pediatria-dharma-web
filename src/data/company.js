eb3114794b2f6e2c2d2c3411aa57f13cdf73762c
jhon-bt <beckperez17@gmail.com>
2026-04-29 21:04:44 -0500

    Ajustes de data

--- src/data/company.js
+++ src/data/company.js
@@ -1,7 +1,7 @@
 // Información del propietario
 export const company = {
   owner: "Darma",
-  name: "Dharma Pediatría",
+  name: "Pediatría Dharma",
   description: "Especialistas en el cuidado avanzado de la salud infantil. En Dharma Pediatría combinamos ciencia, calidez y tecnología para acompañar el crecimiento y bienestar de tus hijos en Ayacucho.",
   tagline: "Especialistas en la salud y bienestar de tus hijos en Ayacucho",
   siteUrl: "https://clinicapediatrica.com", // URL base de tu sitio 
@@ -11,15 +11,16 @@ export const company = {
     district: "San Juan Bautista",
     city: "Huamanga",
     state: "Ayacucho",
-    zip: "05001",
+    zip: "0000",
     country: "Perú",
-    mapLink: "https://maps.app.goo.gl/HY9xLyGiGDvp3ofGA",
     fullAddress:
     "Av. Independencia 123, San Juan Bautista, Huamanga, Ayacucho, Perú",
+    mapLink: "https://maps.app.goo.gl/iS3R6LaLrdkSKqWw9",
+    mapFrame: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d971.2110566565997!2d-74.2054192!3d-13.1722199!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x911287945d7c776b%3A0x989834262cc07273!2sPediatr%C3%ADa%20Dharma!5e0!3m2!1ses-419!2spe!4v1777475090819!5m2!1ses-419!2spe"
   },
   contact: {
     phone: "+51 997 307 782", // Manteniendo la línea de contacto central
-    email: "pediatria@dharma.com.pe",
+    email: "dharmatabiat@gmail.com",
     whatsapp: "+51 997 307 782",
     linkWhatsapp: "https://wa.me/51997307782"
   },
